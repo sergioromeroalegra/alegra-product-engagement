fe6a88d018a1280a2e69c45a50bfafdb5d16fe30
sergioromeroalegra <sergio.romero@alegra.com>
2025-12-02 15:36:17 -0500

    Create obtener_sign_ups.sql

---
+++ queries_base/obtener_sign_ups.sql
@@ -0,0 +1,10 @@
+SELECT
+app_version AS country
+,id_company
+,TO_DATE(CAST(id_date_registration_alegra AS VARCHAR), 'YYYYMMDD') AS sign_up_date
+,segment_type_def AS id_company_segment --Segmento Core / Lite
+FROM dwh_facts.fact_sign_ups 
+WHERE app_version IN ('colombia','costaRica','republicaDominicana','mexico','argentina','peru')
+    AND event_type = 'LOGO' --Primer Sign Up
+    AND id_product = 1 --Alegra Contabilidad--
+    AND id_date_registration_alegra BETWEEN 20250601 AND 20251031
