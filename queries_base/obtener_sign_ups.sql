b86fce260bec5a07d8dc23090cb32262e289e8cc
sergioromeroalegra <sergio.romero@alegra.com>
2025-12-02 15:37:16 -0500

    Update obtener_sign_ups.sql
    Variable rango de fechas para evitar errores

--- queries_base/obtener_sign_ups.sql
+++ queries_base/obtener_sign_ups.sql
@@ -7,4 +7,4 @@ FROM dwh_facts.fact_sign_ups
 WHERE app_version IN ('colombia','costaRica','republicaDominicana','mexico','argentina','peru')
     AND event_type = 'LOGO' --Primer Sign Up
     AND id_product = 1 --Alegra Contabilidad--
-    AND id_date_registration_alegra BETWEEN 20250601 AND 20251031
+    AND id_date_registration_alegra BETWEEN {fecha_inicio} AND {fecha_fin}
