7d69a327b41df099105b8c0c8d2c6da89f5133b9
sergioromeroalegra <sergio.romero@alegra.com>
2025-12-17 17:16:40 -0500

    Update and rename obtener_sign_ups.sql to user_sign_ups.sql

--- queries_base/user_sign_ups.sql
+++ queries_base/user_sign_ups.sql
@@ -9,4 +9,4 @@ FROM dwh_facts.fact_sign_ups
 WHERE app_version IN ('colombia','costaRica','republicaDominicana','mexico','argentina','peru')
     AND event_type = 'LOGO' --Primer Sign Up
     AND id_product = 1 --Alegra Contabilidad--
-    AND id_date_registration_alegra BETWEEN {fecha_inicio} AND {fecha_fin}
+    AND id_date_registration_alegra BETWEEN {fecha_inicio} AND {fecha_fin} -- sin comillas
